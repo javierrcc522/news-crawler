f6c52633e8a3344027b15e22b2aeb348903bc30a
javier jimenez and Sarah Lindmar <javier@>
2017-10-18 16:51:11 -0700

    add two more api requests, hacker news request works

--- js/news-crawler-interface.js
+++ js/news-crawler-interface.js
@@ -3,7 +3,7 @@ import { News } from './../js/news-crawler.js';
 $(document).ready(function() {
   $('#bbcNews').click(function() {
     //dont run below code until the (promise) is resolved
-    let promise = new Promise(function(resolve, reject) {
+    let promise1 = new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
       let url = `https://newsapi.org/v1/articles?source=bbc-news&sortBy=top&apiKey=485e73d110004d07b9d8cd9a0dac9893`;
       request.onload = function() {
@@ -16,17 +16,81 @@ $(document).ready(function() {
       request.open("GET", url, true);
       request.send();
     });
-    promise.then(function(response) {
+
+    promise1.then(function(response) {
       let body = JSON.parse(response);
       let arr = [];
       body.articles.forEach(function(article) {
         arr.push(article);
-      })
+      });
       arr.forEach(function(article) {
           $('#topBBC').append(`<li><a href='${article.url}'</a>${article.title}</li>`);
       }, function(error) {
           $('.showErrors').text(`There was an error processing your request: ${error.message}`);
       });
-    })
+    });
+  });
+
+  $('#hackerNews').click(function() {
+    //dont run below code until the (promise) is resolved
+    let promise2 = new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      let url = `https://newsapi.org/v1/articles?source=hacker-news&sortBy=top&apiKey=485e73d110004d07b9d8cd9a0dac9893`;
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
+        }
+      };
+      request.open("GET", url, true);
+      request.send();
+    });
+
+    promise2.then(function(response) {
+      let body = JSON.parse(response);
+      let arr = [];
+      body.articles.forEach(function(article) {
+        arr.push(article);
+      });
+      arr.forEach(function(article) {
+          $('#topHacker').append(`<li><a href='${article.url}'</a>${article.title}</li>`);
+      }, function(error) {
+          $('.showErrors').text(`There was an error processing your request: ${error.message}`);
+      });
+    });
+  });
+
+  $('#categoryButton').click(function() {
+    let category = $('#category').val();
+    //dont run below code until the (promise) is resolved
+    let promise3 = new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      let url = `https://newsapi.org/v1/sources?category`;
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
+        }
+      };
+      request.open("GET", url, true);
+      request.send();
+    });
+
+    promise3.then(function(response) {
+      let body = JSON.parse(response);
+      let arr = [];
+      body.sources.forEach(function(source) {
+        if(`${source.category}` === category) {
+          arr.push(source);
+        }
+      });
+      arr.forEach(function(category) {
+          $('#newsCategory').append(`<li><a href='${source.url}'</a>${source.description}</li>`);
+      }, function(error) {
+          $('.showErrors').text(`There was an error processing your request: ${error.message}`);
+      });
+    });
   });
 });
