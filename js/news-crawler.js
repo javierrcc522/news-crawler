4766bbbb6a70f62ec5a82f523b9a64e0cd286b2e
Javier Jimenez and Sarah Lindmar <javier@>
2017-10-19 15:38:49 -0700

    add .env to hide api key

--- js/news-crawler.js
+++ js/news-crawler.js
@@ -1,25 +1,9 @@
+var apiKey = require('./../.env').apiKey;
 export class News {
-  // newsSourceApi(){
-  //   return  new Promise(function(resolve, reject) {
-  //     let request = new XMLHttpRequest();
-  //     let url = `https://newsapi.org/v1/articles?source=bbc-news&sortBy=top&apiKey=485e73d110004d07b9d8cd9a0dac9893`;
-  //
-  //     request.onload = function() {
-  //       if (this.status === 200) {
-  //         resolve(request.response);
-  //       } else {
-  //         reject(Error(request.statusText));
-  //       }
-  //     };
-  //     request.open("GET", url, true);
-  //     request.send();
-  //   });
-  // }
-
   newsSourceApi(newssrc){
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-      let url = `https://newsapi.org/v1/articles?source=${newssrc}&sortBy=top&apiKey=485e73d110004d07b9d8cd9a0dac9893`;
+      let url = `https://newsapi.org/v1/articles?source=${newssrc}&sortBy=top&apiKey=${apiKey}`;
 
       request.onload = function() {
         if (this.status === 200) {
