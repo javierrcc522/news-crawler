eebc14f5fe9b9829c9713e6542e9ef13987c256b
Javier Jimenez and Sarah Lindmar <sarah@>
2017-10-19 10:56:44 -0700

    seperate api front end and back end

--- js/news-crawler-interface.js
+++ js/news-crawler-interface.js
@@ -1,23 +1,12 @@
 import { News } from './../js/news-crawler.js';
 
+
 $(document).ready(function() {
   $('#bbcNews').click(function() {
-    //dont run below code until the (promise) is resolved
-    let promise1 = new Promise(function(resolve, reject) {
-      let request = new XMLHttpRequest();
-      let url = `https://newsapi.org/v1/articles?source=bbc-news&sortBy=top&apiKey=485e73d110004d07b9d8cd9a0dac9893`;
-      request.onload = function() {
-        if (this.status === 200) {
-          resolve(request.response);
-        } else {
-          reject(Error(request.statusText));
-        }
-      };
-      request.open("GET", url, true);
-      request.send();
-    });
+    let news = new News();
+    news.newsSourceApi().then(greeting);
 
-    promise1.then(function(response) {
+    function greeting(response){
       let body = JSON.parse(response);
       let arr = [];
       body.articles.forEach(function(article) {
@@ -28,37 +17,7 @@ $(document).ready(function() {
       }, function(error) {
           $('.showErrors').text(`There was an error processing your request: ${error.message}`);
       });
-    });
-  });
-
-  $('#hackerNews').click(function() {
-    //dont run below code until the (promise) is resolved
-    let promise2 = new Promise(function(resolve, reject) {
-      let request = new XMLHttpRequest();
-      let url = `https://newsapi.org/v1/articles?source=hacker-news&sortBy=top&apiKey=485e73d110004d07b9d8cd9a0dac9893`;
-      request.onload = function() {
-        if (this.status === 200) {
-          resolve(request.response);
-        } else {
-          reject(Error(request.statusText));
-        }
-      };
-      request.open("GET", url, true);
-      request.send();
-    });
-
-    promise2.then(function(response) {
-      let body = JSON.parse(response);
-      let arr = [];
-      body.articles.forEach(function(article) {
-        arr.push(article);
-      });
-      arr.forEach(function(article) {
-          $('#topHacker').append(`<li><a href='${article.url}'</a>${article.title}</li>`);
-      }, function(error) {
-          $('.showErrors').text(`There was an error processing your request: ${error.message}`);
-      });
-    });
+    }
   });
 
   $('#categoryButton').click(function() {
--- js/news-crawler.js
+++ js/news-crawler.js
@@ -1,6 +1,21 @@
 export class News {
-  constructor() {
+  newsSourceApi(){
+    return  new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      let url = `https://newsapi.org/v1/articles?source=bbc-news&sortBy=top&apiKey=485e73d110004d07b9d8cd9a0dac9893`;
+
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
+        }
+      };
+      request.open("GET", url, true);
+      request.send();
+    });
 
   }
+    
 
 }
